45b947e297a7f0bb346d14b8caa506c5e38634f8
kingavatar <saikiranreddy06@gmail.com>
2021-03-12 00:18:25 +0530

    Added Calculator and EntryPoint

---
+++ src/main/java/com/github/kingavatar/scientificcalculator/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+    private static final String message = "This is a Calculator CLI Application";
+    Calculator(){
+        
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
---
+++ src/main/java/com/github/kingavatar/scientificcalculator/EntryPoint.java
@@ -0,0 +1,7 @@
+package com.github.kingavatar.scientificcalculator;
+public class EntryPoint {
+    public static void main(String[] args) {
+        Calculator scientificCalculator = new Calculator();
+        System.out.println(scientificCalculator);
+    }
+}
